36725a72281ac8f9c19c8b194e9f153d820cf04a
xwartz <stddup@gmail.com>
2015-10-17 11:08:34 +0800

    gulp 脚本配置

---
+++ dist/smartbanner.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var defaults = {
+    icon: null, // the url of the app icon
+    title: null, // the title of the app
+    author: null, // the author of the app
+    appStore: null, // the url of app for iOS
+    marketUrl: null, // the url of app for Android
+    schemeIOS: null, // the scheme url for iOS
+    schemeAndroid: null, // the scheme url for Android
+    intentUrl: null, // the intent url for chrome 25+
+    button: 'OPEN' // the button text
+};
